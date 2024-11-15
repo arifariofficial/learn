df0c6622dc76f425f64f9e0e67fcaca7cc0946bc
Ariful Islam <ariful6@yahoo.com>
2024-11-15 16:26:02 +0200

    init

---
+++ java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    private final  String  description;
+    private   boolean  isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return  isCompleted;
+    }
+
+    public void markAsCompleted() {
+        isCompleted = true;
+    }
+
+
+    @Override
+    public String toString() {
+        return (isCompleted ? "[isCompleted] " : "[Pending] ") + description;
+    }
+}
