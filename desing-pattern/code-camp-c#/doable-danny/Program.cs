850823c6144919a1eb1cc561dad7a40ed326bba9
Ariful Islam <ariful6@yahoo.com>
2025-01-07 02:15:25 +0200

    encapsulation

---
+++ desing-pattern/code-camp-c#/doable-danny/Program.cs
@@ -0,0 +1,13 @@
+﻿using doable_danny.src.OopPrinciples.Encapsulation;
+
+BankAccount bankAccount = new BankAccount(100);
+
+Console.WriteLine($"Initial balance: {bankAccount.getBalance()}");
+
+bankAccount.Deposit(50);
+
+Console.WriteLine($"Balance after deposit: {bankAccount.getBalance()}");
+
+bankAccount.Withdraw(100);
+
+Console.WriteLine($"Balance after withdrawal: {bankAccount.getBalance()}");
