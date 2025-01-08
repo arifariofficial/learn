12eb6ade1ef324486884ec6e976a25cb6ab2286f
Ariful Islam <ariful6@yahoo.com>
2025-01-08 04:20:02 +0200

    bankAccount

---
+++ desing-pattern/design-patterns-in-C-sharp/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp.OopPrinciples.Encapsulation;
+
+BankAccount bankAccount = new BankAccount(100);
+
+Console.WriteLine($"Initial balance: {bankAccount.getBalance()}");
+
+bankAccount.Deposit(50);
+
+Console.WriteLine($"After depositing 50: {bankAccount.getBalance()}");
+
+bankAccount.Withdraw(100);
+
+Console.WriteLine($"After withdrawing 100: {bankAccount.getBalance()}");
