a2ed075c051a09b2c3eb013644e3f06cad28b53f
Ariful Islam <ariful6@yahoo.com>
2025-01-08 04:18:18 +0200

    bankAccount

---
+++ desing-pattern/design-patterns-in-C-sharp/ConsoleApp/ConsoleApp/src/OopPrinciples/Encapsulation/BadBankAccount.cs
@@ -0,0 +1,6 @@
+namespace ConsoleApp.OopPrinciples.Encapsulation;
+
+public class BadBankAccount
+{
+    public decimal balance;
+}
---
+++ desing-pattern/design-patterns-in-C-sharp/ConsoleApp/ConsoleApp/src/OopPrinciples/Encapsulation/BankAccount.cs
@@ -0,0 +1,41 @@
+namespace ConsoleApp.OopPrinciples.Encapsulation;
+
+public class BankAccount
+{
+    private decimal balance;
+
+    public decimal getBalance()
+    {
+        return this.balance;
+    }
+
+    public BankAccount(decimal balance)
+    {
+        Deposit(balance);
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException("Deposit amount must be greater than 0");
+        }
+
+        this.balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException("Withdrawal amount must be greater than 0");
+        }
+
+        if (amount > this.balance)
+        {
+            throw new InvalidOperationException("Insufficient funds");
+        }
+
+        this.balance -= amount;
+    } 
+}
